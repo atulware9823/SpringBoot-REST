97f9e9e0c02931ede283277c62cfb979d76eb245
Atul Ware <eratulware@gmail.com>
2025-08-14 00:16:56 +0530

    Spring boot REST API web app

---
+++ src/test/java/com/brickstone/SpringbootrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brickstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
